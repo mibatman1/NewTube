879716e23fb89c1ee3a6010d89825ead20ab0f7e
SHIVAM <66750974+mibatman1@users.noreply.github.com>
2023-10-17 11:25:45 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-const apiKey='AIzaSyC_ptT0ViYurdtZqwea6118njEELGxYVnM'
+const apiKey=''
 export const api='https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&&maxResults=50&regionCode=IN&key='+apiKey;
 
 export const searchAPI='http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q='
